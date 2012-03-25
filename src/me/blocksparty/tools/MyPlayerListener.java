40c4a7c1391d0c82ca00708c9a91d294902f2883
BlocksParty <post@blocksparty.net>
2012-03-25 03:27:59 +0200

    Commit 2;
    - Implemented 2 new tools!

--- src/me/blocksparty/tools/MyPlayerListener.java
+++ src/me/blocksparty/tools/MyPlayerListener.java
@@ -4,32 +4,119 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.ChatColor;
-import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerItemHeldEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.material.MaterialData;
+import org.bukkit.util.Vector;
 
 public class MyPlayerListener implements Listener{
 	public static Tools plugin;
 	
+	Map<String, String> bptools = Tools.bptools;
+	public static Map<String, String> autopilot = new HashMap<String, String>();
+	public static Map<String, String> normal = new HashMap<String, String>();
+	public static Map<String, String> high = new HashMap<String, String>();
+
 	@EventHandler
-	public void handle(PlayerInteractEvent event) {
-	ItemStack is = event.getItem();
-	ItemStack beans = new MaterialData(Material.INK_SACK, (byte) 3).toItemStack();
-	if (is.equals(beans)) {
+	public void onPlayerInteract(PlayerInteractEvent event){
 		Player player = event.getPlayer();
 		
-		player.sendMessage("YOU ARE FUNNY!");
+		if (bptools.isEmpty()) return;
+		if (!event.hasItem()) return;
+		
+			MaterialData materialdata = event.getItem().getData();
+			MaterialData inksac = new MaterialData(Material.INK_SACK, (byte) 0);
+			MaterialData rosered = new MaterialData(Material.INK_SACK, (byte) 1);
+
+		if(materialdata.equals(inksac)){
+				if(event.getAction() == Action.RIGHT_CLICK_AIR){
+					if(!autopilot.containsKey(player.getName())){
+						autopilot.put(player.getName(), "autopilot");
+						player.sendMessage(ChatColor.LIGHT_PURPLE + "The AutoPilot is Enable, and the speed is set to; Slow!");
+						event.setCancelled(true);
+					}else if(autopilot.containsKey(player.getName())){
+						if(!normal.containsKey(player.getName())){
+							normal.put(player.getName(), "speed");
+							player.sendMessage(ChatColor.LIGHT_PURPLE + "The AutoPilot speed is set to; Normal!");
+						}else if(normal.containsKey(player.getName()) && !high.containsKey(player.getName())){
+							high.put(player.getName(), "speed");
+							player.sendMessage(ChatColor.LIGHT_PURPLE + "The AutoPilot speed is set to; High!");
+						}else if(normal.containsKey(player.getName()) && high.containsKey(player.getName())){
+							normal.remove(player.getName());
+							high.remove(player.getName());
+							player.sendMessage(ChatColor.LIGHT_PURPLE + "The AutoPilot speed is set to; Slow!");
+						}
+				}
+			}else if(event.getAction() == Action.LEFT_CLICK_AIR){
+					if(autopilot.containsKey(player.getName())){
+						autopilot.remove(player.getName());
+						normal.remove(player.getName());
+						high.remove(player.getName());
+						player.sendMessage(ChatColor.LIGHT_PURPLE + "The AutoPilot is Disable!");
+						event.setCancelled(true);
+				}
+			}
+		}else if(materialdata.equals(rosered)){
+				if(event.getAction() == Action.LEFT_CLICK_AIR){
+					Vector newVec = player.getLocation().getDirection().multiply(10.0);
+		            newVec.setY(newVec.getY()/1.1);
+		            player.setVelocity(newVec);
+		            player.sendMessage(ChatColor.LIGHT_PURPLE + "Svosj!");
+		            event.setCancelled(true);
+				}else if(event.getAction() == Action.RIGHT_CLICK_AIR){
+					Block targetblock = player.getTargetBlock(null, 50);
+						if(targetblock instanceof Block){
+							Block block = targetblock.getRelative(BlockFace.UP);
+							Location location = block.getLocation();
+							location.setYaw(player.getLocation().getYaw());
+							location.setPitch(player.getLocation().getPitch());
+							player.teleport(location);
+							player.sendMessage(ChatColor.LIGHT_PURPLE + "Svisj!");
+							event.setCancelled(true);
+						}
+				}
+			
+			
+		}
 	}
+	
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent event){
+		Player player = event.getPlayer();
+		
+		if(autopilot.containsKey(player.getName())){
+			if(!normal.containsKey(player.getName())){
+				Vector newVec = player.getLocation().getDirection().multiply(0.5);
+				newVec.setY(newVec.getY()/1.1);
+				player.setVelocity(newVec);
+			}else if(normal.containsKey(player.getName()) && !high.containsKey(player.getName())){
+				Vector newVec = player.getLocation().getDirection().multiply(2.5);
+				newVec.setY(newVec.getY()/1.1);
+				player.setVelocity(newVec);
+			}else if(normal.containsKey(player.getName()) && high.containsKey(player.getName())){
+				Vector newVec = player.getLocation().getDirection().multiply(5.0);
+				newVec.setY(newVec.getY()/1.1);
+				player.setVelocity(newVec);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onEntityDamage(EntityDamageEvent event){
+		if(event.getEntity() instanceof Player){
+			Player player = (Player) event.getEntity();
+		    	if(autopilot.containsKey(player.getName())){
+		    		event.setCancelled(true);
+		    	}
+			}
 	}
 }
--- src/me/blocksparty/tools/Tools.java
+++ src/me/blocksparty/tools/Tools.java
@@ -4,12 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -26,8 +22,6 @@ public class Tools extends JavaPlugin{
 		
 		public static Map<String, String> bptools = new HashMap<String, String>();
 		public static Map<String, String> secret = new HashMap<String, String>();
-		//Map<Player, Material> blockpainting = MyPlayerListener.blockpainting;
-		//Map<Player, Material> nothingpainting = MyPlayerListener.nothingpainting;
 		
 		@Override
 		public void onDisable() {
@@ -55,25 +49,19 @@ public class Tools extends JavaPlugin{
 							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": /bptools list" + " | " + ChatColor.BLUE + "The Tools List" + ChatColor.WHITE + "!");
 							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": /bptools enable" + " | " + ChatColor.BLUE + "Enable The Plugin" + ChatColor.WHITE + "!");
 							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": /bptools disable" + " | " + ChatColor.BLUE + "Disable The Plugin" + ChatColor.WHITE + "!");
-							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": /bptools nportal" + " | " + ChatColor.BLUE + "Create NeatherPortal Infront Of You" + ChatColor.WHITE + "!");
+							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": /bptools reload" + " | " + ChatColor.BLUE + "Reload The Plugin" + ChatColor.WHITE + "!");
 						
 						}else if(args[0].equalsIgnoreCase("list") || args[0].equalsIgnoreCase("l")){	
 							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": Welcome to BPTools, " + ChatColor.BLUE + "List" + ChatColor.WHITE + "!");
-							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": (WOOD_HOE) The Instant Block Break Tool; " + " | " + ChatColor.BLUE + "Breaks The Block You Clicked, Instantly" + ChatColor.WHITE + "!");
-							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": (STONE_HOE) The Give Me Tool; " + " | " + ChatColor.BLUE + "Gives You The Block, You Clicked" + ChatColor.WHITE + "!");
-							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": (IRON_HOE) The Block Painting Tool; " + " | " + ChatColor.BLUE + "Change The Block, You LEFT Clicked To The Block You RIGHT Clicked" + ChatColor.WHITE + "!");
-							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": (GOLD_HOE) The Nothing Painting Tool; " + " | " + ChatColor.BLUE + "Change The Block ABOVE, The Block, You LEFT Clicked To The Block You RIGHT Clicked" + ChatColor.WHITE + "!");
+							player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": (INK_SAC) The AutoPilot Tool; " + " | " + ChatColor.BLUE + "Turn On/Off AutoPilot" + ChatColor.WHITE + "!");
 
 						}else if(args[0].equalsIgnoreCase("enable") || args[0].equalsIgnoreCase("e")){
 							if(!bptools.containsKey(player.getName())){
 								bptools.put(player.getName(), "bptools");
-									ItemStack wood = new ItemStack(Material.WOOD_HOE, 1);
-									ItemStack stone = new ItemStack(Material.STONE_HOE, 1);
-									ItemStack iron = new ItemStack(Material.IRON_HOE, 1);
-									ItemStack gold = new ItemStack(Material.GOLD_HOE, 1);
-									ItemStack greenWool = new ItemStack(Material.WOOL, 1);
-									ItemStack diamond = new ItemStack(Material.DIAMOND_HOE, 1);
-										player.getInventory().addItem(wood, stone, iron, gold, diamond);
+								
+									ItemStack inksack = new ItemStack(Material.INK_SACK, (byte) 0);
+									
+									player.getInventory().addItem(inksack);
 											player.updateInventory();
 												player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": The Tools Is " + ChatColor.BLUE + "ENABLE" + ChatColor.WHITE + "!");
 							}else{
@@ -90,8 +78,6 @@ public class Tools extends JavaPlugin{
 							
 						}else if(args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("r")){
 							bptools.clear();
-							//blockpainting.clear();
-							//nothingpainting.clear();
 							secret.clear();
 								player.sendMessage(ChatColor.GREEN + "[BPTools]" + ChatColor.WHITE + ": The Plugin Is " + ChatColor.BLUE + "RELOADED" + ChatColor.WHITE + "!");
 						
